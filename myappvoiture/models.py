7a1062ed7a92bcf05a5257226b98ce3ccd292bc2
AhatIrsale <Elasritaha191@gmail.com>
2023-04-17 12:28:29 +0000

    adding affichage modeles

--- myappvoiture/models.py
+++ myappvoiture/models.py
@@ -12,14 +12,20 @@ class Modele(models.Model):
     type = models.CharField(max_length=50)
     version= models.IntegerField()
     cat = models.CharField(max_length=60)
+    def __str__(self):
+        return f"{self.version}"
 
 class Agence(models.Model):
     description =  models.CharField(max_length=50)
     prove = models.CharField(max_length=500)
     slug = models.CharField(max_length=30)
+    def __str__(self):
+        return f"{self.description}"
 
 class Voiture(models.Model):
     matricule = models.CharField(max_length=50)
     modele = models.ForeignKey(Modele,on_delete=models.CASCADE())
     Agence = models.ForeignKey(Agence,on_delete=models.CASCADE())
+    def __str__(self):
+        return f"{self.matricule}"
 # Create your models here.
