e50bfbc20e52c824a8115e6250abfdd76ac878c0
AhatIrsale <Elasritaha191@gmail.com>
2023-04-19 19:03:41 +0000

    Templates Done

--- myappvoiture/urls.py
+++ myappvoiture/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('app/', views.list, name='list'),
+    path('', views.index, name='list'),
+    path('contact', views.contact, name='contact'),
+    path('login', views.login, name='login'),
+    path('carsingle', views.carsingle, name='carsingle'),
+    path('profil', views.profil, name='profil'),
+    path('orders', views.orders, name='orders'),
 ]
--- myappvoiture/views.py
+++ myappvoiture/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, redirect
 
 
-def list(request):
-
+def index(request):
     return render(request,'test.html')
+def contact(request):
+    return render(request,'contact.html')
+def login(request):
+    return render(request,'login.html')
+def carsingle(request):
+    return render(request,'carsingle.html')
+
+def profil(request):
+    return render(request,'Profil.html')
+def orders(request):
+    return render(request,'orders.html')
 # Create your views here.
